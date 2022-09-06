52c4a63443651c5a191490bb1a64e6e979fa5bb7
ashwini pawale <pawaleashwini20@gmail.com>
2022-09-06 10:30:13 +0530

    commit

---
+++ Mvc1_pro/Controllers/FoodController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc1_pro.Controllers
+{
+    public class FoodController : Controller
+    {
+        [HttpGet]
+
+        public IActionResult FoodMenu()
+        {
+            List<String> option = new List<String>() { "select option", "Veg", "Non veg" };
+            ViewData["option"] = new SelectList(option);
+
+            return View();
+        }
+        [HttpPost]
+          public IActionResult FoodMenu(IFormCollection form,ICollection<String>food)
+          {
+              ViewBag.Name = form["fname"];
+              ViewBag.Contact = form["contact"];
+              ViewBag.FoodOrder = food;
+              ViewBag.Option = form["option"];
+              return View("FoodDetails");
+          }
+    
+    }
+}
---
+++ Mvc1_pro/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc1_pro.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public IActionResult PersonalDetails()
+        {
+            List<String> options = new List<string>() { "select option","yes","no"};
+            ViewData["options"] = new SelectList(options);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PersonalDetails(IFormCollection form)
+        {
+            ViewBag.Name = form["fname"];
+            ViewBag.Contact = form["contact"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.Hobbies =form["hobbies"];
+            ViewBag.Option = form["options"];
+            return View("Details");
+        }
+    }
+}
